576fff8a439f813407da20fbebbe0ec64c2a0696
naeemtechlyn <naeem.techlyn@gmail.com>
2024-06-05 17:36:51 +0600

    working on home-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,12 +17,12 @@
         });
 
         // accordion
-        $(".accordion-list > li.active .answer").show();
-        $('.accordion-list > li > .answer').hide();
-    
+        $(".accordion-list > li:first-child").addClass("active").find(".answer").show();
+        $('.accordion-list > li:not(:first-child) .answer').hide();
+
         $('.accordion-list > li').click(function() {
             if ($(this).hasClass("active")) {
-                    $(this).removeClass("active").find(".answer").slideUp();
+                $(this).removeClass("active").find(".answer").slideUp();
             } else {
                 $(".accordion-list > li.active .answer").slideUp();
                 $(".accordion-list > li.active").removeClass("active");
@@ -115,6 +115,31 @@
             },
             ]
         });
+
+        // magnific popup init
+        $(".popup-gallery").magnificPopup({
+            delegate: '.popup-img',
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+        });
+
+        $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+            type: "iframe",
+            mainClass: "mfp-fade",
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+
+        // navbar home-2
+        $('.nav-bar').on('click', function(){
+            $('.main-menu').toggleClass('active');
+        })
+        $('.nav-bar').on('click', function(){
+            $('.nav-bar').toggleClass('active');
+        })
 
     });
 
